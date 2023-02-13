971fd6c578fde1285f6b5ad4484fa989c58532ba
swxctx <swxctx@gmail.com>
2023-02-13 13:43:17 +0800

    add huawei extra

--- pushfactory.go
+++ pushfactory.go
@@ -27,5 +27,25 @@ type (
 	}
 
 	HuaweiExtra struct {
+		// 消息类型- LOW：资讯营销类 - NORMAL：服务与通讯类 注：资讯营销类的消息提醒方式为静默通知，仅在下拉通知栏展示。 服务与通讯类的消息提醒方式为锁屏+铃声+震动
+		Importance string `json:"importance,omitempty"`
+		// 作用一：完成自分类权益申请后，用于标识消息类型，确定消息提醒方式，对特定类型消息加快发送，
+		// 取值如下：
+		//	IM：即时聊天
+		//	VOIP：音视频通话
+		//	SUBSCRIPTION：订阅
+		//	TRAVEL：出行
+		//	HEALTH：健康
+		//	WORK：工作事项提醒
+		//	ACCOUNT：帐号动态
+		//	EXPRESS：订单&物流
+		//	FINANCE：财务
+		//	DEVICE_REMINDER：设备提醒
+		//	SYSTEM_REMINDER：系统提示
+		//	MAIL：邮件
+		//	PLAY_VOICE：语音播报（仅透传消息支持）
+		//	MARKETING：内容推荐、新闻、财经动态、生活资讯、社交动态、调研、产品促销、功能推荐、运营活动（仅对内容进行标识，不会加快消息发送）
+		// 作用二：申请特殊权限后，用于标识高优先级透传场景，取值如下： VOIP：音视频通话 PLAY_VOICE：语音播报
+		Category string `json:"category,omitempty"`
 	}
 )
